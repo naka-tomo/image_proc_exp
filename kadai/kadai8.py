8efd693daeb1339067d33d538c7902a912bb463c
naka_t <naka.t06+atlassian@gmail.com>
2018-04-13 17:27:34 +0900

    for文とヒストグラムを追加

--- kadai/kadai8.py
+++ kadai/kadai8.py
@@ -7,11 +7,18 @@ from numpy import *
 # 画像の読み込み
 gazo = imread( "kadai8.bmp", 0 )
 
+# ヒストグラムを表示
+figure()
+hist( gazo.flatten(), 256, (0,255) )
 
-# 課題： 6つの図形の輪郭線の長さ（画素数）を計算しなさい．
-#      レポートでは計算方法を分かりやすく説明しなさい．
+for y in range(0,360):
+    for x in range(0,600):
+        # 課題： 6つの図形の輪郭線の長さ（画素数）を計算しなさい．
+        #      レポートでは計算方法を分かりやすく説明しなさい．
+        gazo[y][x] = gazo[y][x]
 
 
 # 画素値を表示
+figure()
 imshow(gazo, cmap="gray", vmin=0, vmax=255)
 show()
